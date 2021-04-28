c8e453b379904ab9abbc65053a1cbad90174d9d7
munyvuth <munyvuth126@gmail.com>
2021-04-28 11:26:59 -0700

    Making some changes

--- app/src/main/java/com/example/simpletodo/EditActivity.java
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -10,28 +10,28 @@ import android.widget.EditText;
 
 public class EditActivity extends AppCompatActivity {
 
-    Button saveButton;
-    EditText edit;
+    Button btnSave;
+    EditText etUpdatedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
-        saveButton = findViewById(R.id.saveButton);
-        edit = findViewById(R.id.editText);
+        btnSave = findViewById(R.id.btnSave);
+        etUpdatedItem = findViewById(R.id.etUpdatedItem);
 
         getSupportActionBar().setTitle("Edit item");
-        edit.setText(getIntent().getStringExtra(MainActivity.KEY_TEXT));
+        etUpdatedItem.setText(getIntent().getStringExtra(MainActivity.KEY_TEXT));
 
         //after clicking, return back
-        saveButton.setOnClickListener(new View.OnClickListener() {
+        btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //create an intent to handle results
                 Intent result = new Intent();
                 //pass edited data
-                result.putExtra(MainActivity.KEY_TEXT, edit.getText().toString());
+                result.putExtra(MainActivity.KEY_TEXT, etUpdatedItem.getText().toString());
                 result.putExtra(MainActivity.KEY_POS, getIntent().getExtras().getInt(MainActivity.KEY_POS));
                 //set result of intent
                 setResult(RESULT_OK, result);
